c910fd2b280638514ecfa12b69ea76d01513f964
Ilya <klyukovskiy@yandex.ru>
2019-12-24 14:33:25 +0300

    tabs

---
+++ readme.md
@@ -0,0 +1,5 @@
+Рабочие табы для любого проекта
+Необходимо подставить в параметры функции вместо tabsParent: обертку табов, вместо tabs: сами табы, вместо tabsContent: контент в табах.
+
+Work tabs for any project
+It is necessary to substitute tabsParent: wrapper of the tabs in the function parameters, instead of tabs: the tabs themselves, instead of tabsContent: content in the tabs.
--- script.js
+++ script.js
@@ -1,50 +1,46 @@
-window.addEventListener('DOMContentLoaded',function () { 
+window.addEventListener('DOMContentLoaded', function () {
     'use strict';
-    
-function tab( tabsParent, tabs, tabsContent ) { 
+
+    function tab(tabsParent, tabs, tabsContent) {
 
 
-    let tab = document.querySelectorAll(tabsParent),
-        info = document.querySelector(tabs),
-        tabContent = document.querySelectorAll(tabsContent);
+        let tab = document.querySelectorAll(tabsParent),
+            info = document.querySelector(tabs),
+            tabContent = document.querySelectorAll(tabsContent);
 
-// функция которая скрывает все табы кроме 1го
-function hideTabContent(a) { 
-    for (let index = a; index < tabContent.length; index++) {
-        tabContent[index].classList.remove('show');
-        tabContent[index].classList.add('hide');   
+        // function that hides all tabs except 1st
+        function hideTabContent(a) {
+            for (let index = a; index < tabContent.length; index++) {
+                tabContent[index].classList.remove('show');
+                tabContent[index].classList.add('hide');
+            }
+        }
+
+        hideTabContent(1);
+
+
+        // function that shows a tab with an index instead of b
+        function showTabContent(b) {
+            if (tabContent[b].classList.contains('hide')) {
+                tabContent[b].classList.remove('hide');
+                tabContent[b].classList.add('show');
+            }
+
+        }
+        //when you click on a tab, start a cycle for all tabs and check each tab with the tab that you clicked on if they are equal then hide all tabs and the tab that is equal to the one you clicked on show
+        info.addEventListener('click', function (event) {
+            let target = event.target;
+            if (target && target.classList.contains(tabs)) {
+                for (let index = 0; index < tab.length; index++) {
+                    if (target == tab[index]) {
+                        hideTabContent(0);
+                        showTabContent(index);
+                        break;
+                    }
+
+                }
+            }
+        });
     }
- }
 
- hideTabContent(1);
-
-
- // функция которая показывает таб с индексом вместо b
- function showTabContent(b) { 
-   if (tabContent[b].classList.contains('hide')) {
-    tabContent[b].classList.remove('hide');
-    tabContent[b].classList.add('show');
-   }
-
-  }
-//   при клике на таб запускаем цикл по всем табам и сверяем каждый таб с тем табом на который кликнули если они равны то скрываем все табы а таб который равен по тому на который кликнули показываем
-  info.addEventListener('click', function (event) {
-      let target = event.target;
-      if (target && target.classList.contains(tabs)) {
-          for (let index = 0; index < tab.length; index++) {
-              if (target == tab[index]) {
-                hideTabContent(0);
-                showTabContent(index);
-                break;
-              }
-              
-          }
-      }
-  });
-
-
- }
-   
-    
-
- });
+});
